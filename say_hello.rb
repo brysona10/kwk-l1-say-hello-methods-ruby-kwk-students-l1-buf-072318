51f3f7febb00182dabd681ed1d90d716aa11c68e
brysona10 <addisonbryson10@gmail.com>
2018-07-24 14:38:05 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,6 @@
 # Build your say_hello method here
 
-def say_hello(your_name = "Ruby Programmer", my_name)
-  puts "Hello #{your_name}, I'm #{my_name}."
+def say_hello(your_name = "Ruby Programmer",)
+  puts "Hello #{your_name}"
 end
 say_hello("Addie")
-say_hello("Addie", "Abby")
