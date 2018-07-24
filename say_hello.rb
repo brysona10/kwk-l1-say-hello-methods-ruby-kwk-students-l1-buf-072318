4de410e41fd449e2042006a3d4456d643a0439d8
brysona10 <addisonbryson10@gmail.com>
2018-07-24 14:29:11 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -3,5 +3,5 @@
 def say_hello(your_name, my_name)
   puts "Hello #{your_name}, I'm #{my_name}."
 end
-say_hello( my_name,"addie")
-say_hello( your_name,"abby")
+say_hello( my_name)
+say_hello( your_name)
